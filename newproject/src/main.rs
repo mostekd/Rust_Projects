e01e93e3c7f41868b39633abc40cf5dbd49217ef
Dawid Mostowski <dawid.mostowski06@proton.me>
2025-05-20 13:32:31 +0200

    update

--- newproject/src/main.rs
+++ newproject/src/main.rs
@@ -3,4 +3,11 @@ fn main() {
     for i in 0..5 {
         println!("Liczba: {}", i);
     }
+    let numbers = vec![1, 2, 3, 4, 5];
+    let mut sum = 0;
+    for num in &numbers {
+        sum += num;
+        println!("Dodaję {} do sumy, aktualna suma: {}", num, sum);
+    }
+    println!("Suma wszystkich liczb w wektorze: {}", sum);
 }
